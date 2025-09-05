ba810dea281b2bf9578fdd4b0f49c3f5e8f494be
eliifors <145875619+eliifors@users.noreply.github.com>
2025-09-05 16:45:41 +0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
+
+const app = express();
 
 
 
